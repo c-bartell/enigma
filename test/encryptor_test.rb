85ec96359dd0abe367d792a17a42fb4cd70c828c
Curtis Bartell <cbartell980@gmail.com>
2020-09-20 16:37:39 -0600

    Remove test_it_can_generate_cipher and add test_it_has_attributes

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -2,15 +2,13 @@ require './test/test_helper'
 require './lib/encryptor'
 
 class EncryptorTest < Minitest::Test
-  def test_it_can_generate_cipher
-    assert_instance_of Cipher, Encryptor.generate_cipher
-  end
+  def test_it_has_attributes
+    enigma = mock('Enigma object')
+    shift_generator = mock('ShiftGenerator object')
+    cipher = mock('Cipher object')
+    encryptor = Encryptor.new(enigma, shift_generator)
 
-  # def test_it_has_attributes
-  #   enigma = mock('Enigma object')
-  #   shift_generator = mock('shift_generator')
-  #   encryptor = Encryptor.new(enigma, shift_generator)
-  #
-  #   assert_equal
-  # end
+
+    assert_equal
+  end
 end
