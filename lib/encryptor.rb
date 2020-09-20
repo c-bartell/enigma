b92d32b5f2e90d5e91cc1945b2f3179386b9cb3a
Curtis Bartell <cbartell980@gmail.com>
2020-09-20 17:36:13 -0600

    Remove ShiftGenerator from encryptor

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -2,16 +2,11 @@ require './lib/cipher'
 require './lib/shift_generator'
 
 class Encryptor
-  attr_reader :enigma, :shift_generator, :cipher
+  attr_reader :enigma, :cipher
 
   def initialize(enigma)
     @enigma = enigma
-    @shift_generator = generate_shift_generator
     @cipher = generate_cipher
-  end
-
-  def generate_shift_generator
-    ShiftGenerator.new(self)
   end
 
   def generate_cipher
--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -4,31 +4,16 @@ require './lib/encryptor'
 class EncryptorTest < Minitest::Test
   def test_it_has_attributes
     enigma = mock('Enigma object')
-    shift_generator = mock('ShiftGenerator object')
     cipher = mock('Cipher object')
-    ShiftGenerator.stubs(:new).returns(shift_generator)
     Cipher.stubs(:new).returns(cipher)
     encryptor = Encryptor.new(enigma)
 
     assert_equal enigma, encryptor.enigma
-    assert_equal shift_generator, encryptor.shift_generator
     assert_equal cipher, encryptor.cipher
-  end
-
-  def test_it_can_generate_a_shift_generator
-    skip
-    enigma = mock('Enigma object')
-    cipher = mock('Cipher object')
-    Cipher.stubs(:new).returns(cipher)
-    encryptor = Encryptor.new(enigma)
-
-    assert_instance_of ShiftGenerator, encryptor.shift_generator
   end
 
   def test_it_can_generate_a_cipher
     enigma = mock('Enigma object')
-    shift_generator = mock('ShiftGenerator object')
-    ShiftGenerator.stubs(:new).returns(shift_generator)
     encryptor = Encryptor.new(enigma)
 
     assert_instance_of Cipher, encryptor.cipher
@@ -36,9 +21,7 @@ class EncryptorTest < Minitest::Test
 
   def test_it_can_format_message_string
     enigma = mock('Enigma object')
-    shift_generator = mock('ShiftGenerator object')
     cipher = mock('Cipher object')
-    ShiftGenerator.stubs(:new).returns(shift_generator)
     Cipher.stubs(:new).returns(cipher)
     encryptor = Encryptor.new(enigma)
     message = "HeLLo WOrlD\n"
@@ -49,9 +32,7 @@ class EncryptorTest < Minitest::Test
 
   def test_it_can_request_shifts
     enigma = mock('Enigma object')
-    shift_generator = mock('ShiftGenerator object')
     cipher = mock('Cipher object')
-    ShiftGenerator.stubs(:new).returns(shift_generator)
     Cipher.stubs(:new).returns(cipher)
     encryptor = Encryptor.new(enigma)
     shifts = { A: 3, B: 27, C: 73, D: 20 }
