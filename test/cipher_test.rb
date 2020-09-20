a8f6e0d4821c78cf6a412236df2b5ed85243d1b5
Curtis Bartell <cbartell980@gmail.com>
2020-09-20 13:27:36 -0600

    Add test_it_can_update_shift_values

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -16,6 +16,34 @@ class CipherTest < Minitest::Test
     assert_nil @cipher.d_shift
   end
 
+  def test_it_can_request_shifts
+    shifts = { A: 5, B: 12, C: 14, D: 74 }
+    @encryptor.stubs(:request_shifts).returns(shifts)
+
+    assert_equal shifts, @cipher.request_shifts(['nothing or key and or date'])
+  end
+
+  def test_it_can_set_new_shift_values
+    shifts = { A: 5, B: 12, C: 14, D: 74 }
+    @cipher.set_shifts(shifts)
+
+    assert_equal 5, @cipher.a_shift
+    assert_equal 12, @cipher.b_shift
+    assert_equal 14, @cipher.c_shift
+    assert_equal 74, @cipher.d_shift
+  end
+
+  def test_it_can_update_shift_values
+    shifts = { A: 5, B: 12, C: 14, D: 74 }
+    @encryptor.stubs(:request_shifts).returns(shifts)
+    @cipher.update_shifts(['nothing or key and or date'])
+    
+    assert_equal 5, @cipher.a_shift
+    assert_equal 12, @cipher.b_shift
+    assert_equal 14, @cipher.c_shift
+    assert_equal 74, @cipher.d_shift
+  end
+
   def test_it_can_generate_character_set
     expected = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
       'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
@@ -37,22 +65,6 @@ class CipherTest < Minitest::Test
     assert_equal 'h', @cipher.encrypt_letter('z', 36)
   end
 
-  def test_it_can_set_new_shift_values
-    shifts = { A: 5, B: 12, C: 14, D: 74 }
-    @cipher.set_shifts(shifts)
-
-    assert_equal 5, @cipher.a_shift
-    assert_equal 12, @cipher.b_shift
-    assert_equal 14, @cipher.c_shift
-    assert_equal 74, @cipher.d_shift
-  end
-
-  def test_it_can_request_shifts
-    shifts = { A: 5, B: 12, C: 14, D: 74 }
-    @encryptor.stubs(:request_shifts).returns(shifts)
-
-    assert_equal shifts, @cipher.request_shifts(['nothing or key and or date'])
-  end
   # def test_it_can_encrypt_a_message
   #
   # end
