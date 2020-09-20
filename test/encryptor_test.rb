34b2768af3568e0135ee2d6c202694d781892aa8
Curtis Bartell <cbartell980@gmail.com>
2020-09-20 16:31:32 -0600

    Add test_it_can_generate_cipher

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -2,4 +2,15 @@ require './test/test_helper'
 require './lib/encryptor'
 
 class EncryptorTest < Minitest::Test
+  def test_it_can_generate_cipher
+    assert_instance_of Cipher, Encryptor.generate_cipher
+  end
+
+  # def test_it_has_attributes
+  #   enigma = mock('Enigma object')
+  #   shift_generator = mock('shift_generator')
+  #   encryptor = Encryptor.new(enigma, shift_generator)
+  #
+  #   assert_equal
+  # end
 end
