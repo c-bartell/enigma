62a116efe0750b22ae8fe122f81fa1350ba696c6
Curtis Bartell <cbartell980@gmail.com>
2020-09-20 17:00:43 -0600

    Add initialize and generators

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,5 +1,20 @@
 require './lib/cipher'
+require './lib/shift_generator'
 
 class Encryptor
-  
+  attr_reader :enigma, :shift_generator, :cipher
+
+  def initialize(enigma)
+    @enigma = enigma
+    @shift_generator = generate_shift_generator
+    @cipher = generate_cipher
+  end
+
+  def generate_shift_generator
+    ShiftGenerator.new(self)
+  end
+
+  def generate_cipher
+    Cipher.new(self)
+  end
 end
--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -1,17 +1,19 @@
 require './test/test_helper'
 require './lib/encryptor'
 
-class EncryptorTest < Minitest::Test
+class EncryptorTest < Minitest::Test  
   def test_it_has_attributes
     enigma = mock('Enigma object')
     shift_generator = mock('ShiftGenerator object')
     cipher = mock('Cipher object')
-    Encryptor.stubs(:generate_cipher).returns(cipher)
-    Encryptor.stubs(:generate_shift_generator)
+    ShiftGenerator.stubs(:new).returns(shift_generator)
+    Cipher.stubs(:new).returns(cipher)
     encryptor = Encryptor.new(enigma)
 
     assert_equal enigma, encryptor.enigma
     assert_equal shift_generator, encryptor.shift_generator
-    assert_equal cipher, encryptor.cipher    
+    assert_equal cipher, encryptor.cipher
   end
+
+
 end
