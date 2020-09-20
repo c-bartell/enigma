c3ca67035421772633a61c345726329e4ca082d2
Curtis Bartell <cbartell980@gmail.com>
2020-09-19 20:32:49 -0600

    Add setup

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -2,15 +2,17 @@ require './test/test_helper'
 require './lib/cipher'
 
 class CipherTest < Minitest::Test
+  def setup
+    @cipher = Cipher.new(3)
+  end
+
   def test_it_has_attributes
-    cipher = Cipher.new(3)
-    assert_equal 3, cipher.shift
+    assert_equal 3, @cipher.shift
   end
 
   def test_it_can_generate_character_set
-    cipher = Cipher.new(3)
     expected = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
-    assert_equal expected, cipher.characters
+    assert_equal expected, @cipher.characters
   end
 end
