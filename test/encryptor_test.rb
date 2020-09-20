78821b6ff6e5b9b4821287d287653ba633f25ebf
Curtis Bartell <cbartell980@gmail.com>
2020-09-20 16:42:02 -0600

    Update test_it_has_attributes

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -6,9 +6,12 @@ class EncryptorTest < Minitest::Test
     enigma = mock('Enigma object')
     shift_generator = mock('ShiftGenerator object')
     cipher = mock('Cipher object')
-    encryptor = Encryptor.new(enigma, shift_generator)
+    Encryptor.stubs(:generate_cipher).returns(cipher)
+    Encryptor.stubs(:generate_shift_generator)
+    encryptor = Encryptor.new(enigma)
 
-
-    assert_equal
+    assert_equal enigma, encryptor.enigma
+    assert_equal shift_generator, encryptor.shift_generator
+    assert_equal cipher, encryptor.cipher    
   end
 end
