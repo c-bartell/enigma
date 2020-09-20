7999432288579b921adc043058ad8f675406870c
Curtis Bartell <cbartell980@gmail.com>
2020-09-19 20:28:37 -0600

    Add test_it_can_generate_character_set

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -4,7 +4,12 @@ require './lib/cipher'
 class CipherTest < Minitest::Test
   def test_it_has_attributes
     cipher = Cipher.new(3)
-
     assert_equal 3, cipher.shift
+  end
+
+  def test_it_can_generate_character_set
+    expected = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+       'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+    assert_equal expected, cipher.characters
   end
 end
