e932cf2504c325c057c11af4eda45b638d24b0ea
Curtis Bartell <cbartell980@gmail.com>
2020-09-19 20:12:11 -0600

    Add test_it_has_attributes

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -2,4 +2,11 @@ require './test/test_helper'
 require './lib/cipher'
 
 class CipherTest < Minitest::Test
+  def test_it_has_attributes
+    cipher = Cipher.new(3)
+    chars = ('a..z').to_a << ' '
+
+    assert_equal 3, cipher.shift
+    assert_equal chars, cipher.characters
+  end
 end
