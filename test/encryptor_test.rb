1ae6fc2b534e494ab77bb705ba3887f21df49998
Curtis Bartell <cbartell980@gmail.com>
2020-09-20 17:07:25 -0600

    Add test_it_can_generate_a_cipher

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -18,12 +18,19 @@ class EncryptorTest < Minitest::Test
   def test_it_can_generate_a_shift_generator
     skip
     enigma = mock('Enigma object')
-    shift_generator = mock('ShiftGenerator object')
     cipher = mock('Cipher object')
-    ShiftGenerator.stubs(:new).returns(shift_generator)
     Cipher.stubs(:new).returns(cipher)
     encryptor = Encryptor.new(enigma)
 
     assert_instance_of ShiftGenerator, encryptor.shift_generator
+  end
+
+  def test_it_can_generate_a_cipher
+    enigma = mock('Enigma object')
+    shift_generator = mock('ShiftGenerator object')
+    ShiftGenerator.stubs(:new).returns(shift_generator)
+    encryptor = Encryptor.new(enigma)
+
+    assert_instance_of Cipher, encryptor.cipher
   end
 end
