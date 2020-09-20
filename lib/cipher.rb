93036599ef8dd25ed30f75a01a37f26c57d5c12f
Curtis Bartell <cbartell980@gmail.com>
2020-09-19 22:08:14 -0600

    Prepare to retool Cipher class

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,23 +1,23 @@
 class Cipher
-  attr_reader :shift
-
-  def initialize(shift)
-    @shift = shift
-  end
-
-  def set_shift(value)
-    @shift = value
-  end
-
-  def characters
-    ('a'..'z').to_a << ' '
-  end
-
-  def shifted_characters
-    characters.rotate(shift)
-  end
-
-  def encrypt(letter)
-    shifted_characters[characters.index(letter)]
-  end
+  # attr_reader :shift
+  #
+  # def initialize(shift)
+  #   @shift = shift
+  # end
+  #
+  # def set_shift(value)
+  #   @shift = value
+  # end
+  #
+  # def characters
+  #   ('a'..'z').to_a << ' '
+  # end
+  #
+  # def shifted_characters
+  #   characters.rotate(shift)
+  # end
+  #
+  # def encrypt(letter)
+  #   shifted_characters[characters.index(letter)]
+  # end
 end
