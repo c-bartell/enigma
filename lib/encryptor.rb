b01ab83af0c02d283c8423db80e3af4fc7d8e412
Curtis Bartell <cbartell980@gmail.com>
2020-09-20 18:03:29 -0600

    Add package

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -20,4 +20,12 @@ class Encryptor
   def request_shifts(key_date_data) # key_date_data comes in as [key_string, date_string]
     enigma.request_shifts(key_date_data)
   end
+
+  def package(encrypted, key_date_data)
+    {
+      key: key_date_data[0],
+      date: key_date_data[1],
+      encryption: encrypted.join
+    }
+  end
 end
