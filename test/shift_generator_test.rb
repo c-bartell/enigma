1db6ef54d84f5cd340712f8f2153c6477f8ffe07
Curtis Bartell <cbartell980@gmail.com>
2020-09-21 13:29:35 -0600

    Add test_it_can_calculate_combined_str_length_of_elements

--- test/shift_generator_test.rb
+++ test/shift_generator_test.rb
@@ -9,6 +9,24 @@ class ShiftGeneratorTest < Minitest::Test
     assert_equal enigma, shift_generator.enigma
   end
 
+  def test_it_can_calculate_combined_str_length_of_elements
+    enigma = mock('Enigma object')
+    shift_generator = ShiftGenerator.new(enigma)
+
+    assert_equal 11, shift_generator.combi_length(['02715', '040895'])
+    assert_equal 6, shift_generator.combi_length(['02715', '040895'])
+    assert_equal 5, shift_generator.combi_length(['02715', '040895'])
+    assert_equal 0, shift_generator.combi_length(['02715', '040895'])
+  end
+
+  # def test_it_can_evaluate_incoming_key_date_data
+  #   skip
+  #   enigma = mock('Enigma object')
+  #   shift_generator = ShiftGenerator.new(enigma)
+  #
+  #
+  # end
+
   def test_it_can_validate_key_date_data
     skip
     enigma = mock('Enigma object')
