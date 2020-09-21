9612c56de96793e2bf487d53feeecdabc242e31a
Curtis Bartell <cbartell980@gmail.com>
2020-09-21 12:10:31 -0600

    Add test_it_can_validate_key_date_data

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,4 +12,15 @@ class EnigmaTest < Minitest::Test
     assert_equal encryptor, enigma.encryptor
     assert_equal shift_generator, enigma.shift_generator
   end
+
+  def test_it_can_validate_key_date_data
+    encryptor = mock('Encryptor object')
+    Encryptor.stubs(:new).returns(encryptor)
+    shift_generator = mock('ShiftGenerator object')
+    ShiftGenerator.stubs(:new).returns(shift_generator)
+    enigma = Enigma.new
+    shift_generator.stubs(:key_date).returns(['02715', '040895'])
+
+    assert_equal ['02715', '040895'], enigma.key_date([])
+  end
 end
