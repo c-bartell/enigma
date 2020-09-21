0258eab600391d27c686a56dc814ade9e93a5e16
Curtis Bartell <cbartell980@gmail.com>
2020-09-21 11:59:38 -0600

    Add shift_generator

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,11 @@
+require './lib/encryptor'
+require './lib/shift_generator'
+
 class Enigma
-  attr_reader :encryptor
+  attr_reader :encryptor, :shift_generator
 
   def initialize
     @encryptor = Encryptor.new(self)
+    @shift_generator = ShiftGenerator.new(self)
   end
 end
