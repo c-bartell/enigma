26c7ff405e915a682c8c65845ec48fa51cc9ae6e
Curtis Bartell <cbartell980@gmail.com>
2020-09-21 12:35:52 -0600

    Add test_it_has_attributes

--- test/shift_generator_test.rb
+++ test/shift_generator_test.rb
@@ -2,4 +2,10 @@ require './test/test_helper'
 require './lib/shift_generator'
 
 class ShiftGeneratorTest < Minitest::Test
+  def test_it_has_attributes
+    enigma = mock('Enigma object')
+    shift_generator = ShiftGenerator.new(enigma)
+
+    assert_equal enigma, shift_generator.enigma
+  end
 end
