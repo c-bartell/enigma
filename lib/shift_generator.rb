8946b6ece0a1efb24634d8e8dfc9b31b5b121625
Curtis Bartell <cbartell980@gmail.com>
2020-09-21 13:40:37 -0600

    Add combi_length

--- lib/shift_generator.rb
+++ lib/shift_generator.rb
@@ -4,4 +4,21 @@ class ShiftGenerator
   def initialize(enigma)
     @enigma = enigma
   end
+
+  def combi_length(key_date_data)
+    key_date_data.sum do |datum|
+      datum.length
+    end
+  end
+
+  def key_date(key_date_data)
+    # combined string length: 11, 6, 5, or 0
+    #   11 means all good, return key_date_data
+    #   Not 11 means correct the data
+    # return key_date_data if key_date_data.length == 2
+    # if key_date_data.first.length == 6
+    #   #generate key and add to beginning
+    # elsif key_date_data
+    # require 'pry' ; binding.pry
+  end
 end
