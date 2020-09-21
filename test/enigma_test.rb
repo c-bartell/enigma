5c295c478e3527659ec255ada0d298fbf0cbcdf6
Curtis Bartell <cbartell980@gmail.com>
2020-09-21 11:58:03 -0600

    Update test_it_has_attributes

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,8 +5,11 @@ class EnigmaTest < Minitest::Test
   def test_it_has_attributes
     encryptor = mock('Encryptor object')
     Encryptor.stubs(:new).returns(encryptor)
+    shift_generator = mock('ShiftGenerator object')
+    ShiftGenerator.stubs(:new).returns(shift_generator)
     enigma = Enigma.new
 
     assert_equal encryptor, enigma.encryptor
+    assert_equal shift_generator, enigma.shift_generator
   end
 end
