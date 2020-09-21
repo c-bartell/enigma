cde7897b0053e2569d2e4eb757390c0c56bf5b57
Curtis Bartell <cbartell980@gmail.com>
2020-09-21 17:48:24 -0600

    Add enigmock helper

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -2,38 +2,34 @@ require './test/test_helper'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
-  def test_it_has_attributes
-    encryptor = mock('Encryptor object')
-    Encryptor.stubs(:new).returns(encryptor)
-    shift_generator = mock('ShiftGenerator object')
-    ShiftGenerator.stubs(:new).returns(shift_generator)
-    enigma = Enigma.new
+  def enigmock
+    @encryptor = mock('Encryptor object')
+    Encryptor.stubs(:new).returns(@encryptor)
+    @shift_generator = mock('ShiftGenerator object')
+    ShiftGenerator.stubs(:new).returns(@shift_generator)
+    @enigma = Enigma.new
+  end
 
-    assert_equal encryptor, enigma.encryptor
-    assert_equal shift_generator, enigma.shift_generator
+  def test_it_has_attributes
+    enigmock
+
+    assert_equal @encryptor, @enigma.encryptor
+    assert_equal @shift_generator, @enigma.shift_generator
   end
 
   def test_it_can_validate_key_date_data
-    encryptor = mock('Encryptor object')
-    Encryptor.stubs(:new).returns(encryptor)
-    shift_generator = mock('ShiftGenerator object')
-    ShiftGenerator.stubs(:new).returns(shift_generator)
-    enigma = Enigma.new
-    shift_generator.stubs(:key_date).returns(['02715', '040895'])
+    enigmock
+    @shift_generator.stubs(:key_date).returns(['02715', '040895'])
 
-    assert_equal ['02715', '040895'], enigma.key_date([])
+    assert_equal ['02715', '040895'], @enigma.key_date([])
   end
 
   def test_it_can_request_shifts
-    encryptor = mock('Encryptor object')
-    Encryptor.stubs(:new).returns(encryptor)
-    shift_generator = mock('ShiftGenerator object')
-    ShiftGenerator.stubs(:new).returns(shift_generator)
-    enigma = Enigma.new
+    enigmock
     shifts = { A: 3, B: 27, C: 73, D: 20 }
-    shift_generator.stubs(:convert_to_shifts).returns(shifts)
+    @shift_generator.stubs(:convert_to_shifts).returns(shifts)
 
-      assert_equal shifts, enigma.request_shifts(['02715', '040895'])
+    assert_equal shifts, @enigma.request_shifts(['02715', '040895'])
   end
 
   def test_it_can_encrypt_a_message
