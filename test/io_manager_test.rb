75773dd4f244916c169edceedcc7473a870549df
Curtis Bartell <cbartell980@gmail.com>
2020-09-21 23:27:30 -0600

    Add test_it_can_generate_input_text

--- test/io_manager_test.rb
+++ test/io_manager_test.rb
@@ -1,9 +1,16 @@
 require './test/test_helper'
 require './lib/io_manager'
+
 class IOManagerTest < Minitest::Test
   def setup
     @enigma = mock('Enigma object')
     @io_manager = IOManager.new(@enigma)
+  end
+
+  def write_to_fixture(path, content)
+    file = File.open(path, 'w')
+    file.write(content)
+    file.close
   end
 
   def test_it_exists
@@ -12,6 +19,8 @@ class IOManagerTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal @enigma, @io_manager.enigma
+    assert_equal '', @io_manager.input_text
+    assert_equal '', @io_manager.output_text
   end
 
   def test_it_can_read_input_path
@@ -23,6 +32,16 @@ class IOManagerTest < Minitest::Test
   def test_it_can_read_output_path
     ARGV.replace(['message.txt', 'encrypted.txt'])
 
-    assert_equal 'encrypted.txt', @io_manager.output_path    
+    assert_equal 'encrypted.txt', @io_manager.output_path
+  end
+
+  def test_it_can_generate_input_text
+    path = './test/message_fixture.txt'
+    write_to_fixture(path, 'hello world')
+    @io_manager.stubs(:input_path).returns('./test/message_fixture.txt')
+    @io_manager.get_text
+    write_to_fixture(path, '')
+
+    assert_equal 'hello world', @io_manager.input_text
   end
 end
