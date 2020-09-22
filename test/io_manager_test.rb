7fde549fe0e47d45deb7dacd73762de814c41569
Curtis Bartell <cbartell980@gmail.com>
2020-09-21 22:09:08 -0600

    Add test_it_exists

--- test/io_manager_test.rb
+++ test/io_manager_test.rb
@@ -1,5 +1,9 @@
 require './test/test_helper'
 require './lib/io_manager'
 class IOManagerTest < Minitest::Test
-
+  def test_it_exists
+    enigma = mock('Enigma object')
+    io_manager = IOManager.new(enigma)
+    assert_instance_of IOManager, io_manager
+  end
 end
