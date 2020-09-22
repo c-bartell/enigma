7719cf09c7ca0da339b65bd19e2f5f0b6f919a14
Curtis Bartell <cbartell980@gmail.com>
2020-09-21 22:33:42 -0600

    Add test_it_can_read_input_path

--- test/io_manager_test.rb
+++ test/io_manager_test.rb
@@ -13,4 +13,10 @@ class IOManagerTest < Minitest::Test
   def test_it_has_attributes
     assert_equal @enigma, @io_manager.enigma
   end
+
+  def test_it_can_read_input_path
+    ARGV.replace(['message.txt', 'encrypted.txt'])
+
+    assert_equal 'message.txt', @io_manager.input_path
+  end
 end
