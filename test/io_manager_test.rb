d3d92e4d232d21baccc4b14ba3cd863fdafd9712
Curtis Bartell <cbartell980@gmail.com>
2020-09-21 22:14:18 -0600

    Add test_it_has_attributes

--- test/io_manager_test.rb
+++ test/io_manager_test.rb
@@ -1,9 +1,16 @@
 require './test/test_helper'
 require './lib/io_manager'
 class IOManagerTest < Minitest::Test
+  def setup
+    @enigma = mock('Enigma object')
+    @io_manager = IOManager.new(@enigma)
+  end
+
   def test_it_exists
-    enigma = mock('Enigma object')
-    io_manager = IOManager.new(enigma)
-    assert_instance_of IOManager, io_manager
+    assert_instance_of IOManager, @io_manager
+  end
+
+  def test_it_has_attributes
+    assert_equal @enigma, @io_manager.enigma
   end
 end
