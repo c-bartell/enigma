9ec9b60052a1b544f5c2c90f7b96126f2c664aa1
Curtis Bartell <cbartell980@gmail.com>
2020-09-21 23:41:11 -0600

    Add io text attributes and get_text

--- lib/io_manager.rb
+++ lib/io_manager.rb
@@ -1,8 +1,10 @@
 class IOManager
-  attr_reader :enigma
+  attr_reader :enigma, :input_text, :output_text
 
   def initialize(enigma)
     @enigma = enigma
+    @input_text = ''
+    @output_text = ''
   end
 
   def input_path
@@ -11,5 +13,11 @@ class IOManager
 
   def output_path
     ARGV[1]
+  end
+
+  def get_text
+    @input_text = File.open(input_path, 'r') do |file|
+      file.read
+    end
   end
 end
